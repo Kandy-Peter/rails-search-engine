6d2167e62a3ce74d1be49fe3b44c44d7c97fba94
kandy <kandypeter03@gmail.com>
2022-12-05 23:51:01 +0200

    Fix bug on loading charts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,16 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-// require("@rails/ujs").start();
-// require("turbolinks").start();
-// require("@rails/activestorage").start();
-// require("channels");
-require("chartkick");
-require("chart.js");
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import "chartkick/chart.js"
 import "channels"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
+Chartkick.use(Chart)
